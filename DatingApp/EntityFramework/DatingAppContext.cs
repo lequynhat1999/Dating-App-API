355b5a2d50dd1ef275fa5c98c63e2d7e7e673a08
lequynhat1999 <lequynhat1999@gmail.com>
2025-02-15 20:03:57 +0700

    init project

---
+++ DatingApp/EntityFramework/DatingAppContext.cs
@@ -0,0 +1,12 @@
+﻿using DatingApp.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.EntityFramework
+{
+    public class DatingAppContext: DbContext
+    {
+        public DatingAppContext(DbContextOptions<DatingAppContext> options) : base(options) { }
+        
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
