1ba03459bf7b4904b524a1dc1965e046d9500930
lequynhat1999 <lequynhat1999@gmail.com>
2025-02-15 20:30:01 +0700

    adding User Controller

---
+++ DatingApp/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp.EntityFramework;
+using DatingApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly DatingAppContext _context;
+
+        public UserController(DatingAppContext context) {
+            _context = context;    
+        }
+        [HttpGet]
+        public async Task<ServiceResponse> GetUsers()
+        {
+            var res = new ServiceResponse();
+            res.Data = await _context.Users.ToListAsync();
+            return res;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ServiceResponse> GetUser(int id)
+        {
+            var res = new ServiceResponse();
+            res.Data = await _context.Users.FindAsync(id);
+            return res;
+        }
+    }
+}
---
+++ DatingApp/Model/ServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace DatingApp.Model
+{
+    public class ServiceResponse
+    {
+        public bool IsSuccess { get; set; } = true;
+        public string ErrorMessage { get; set; } = "";
+        public object Data { get; set; }
+        public int ErrorCode { get; set; } = 200;
+    }
+}
